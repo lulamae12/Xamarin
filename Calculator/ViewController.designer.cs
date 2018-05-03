1c49202717e0eb1c9caca4fafde1c2c02e580e73
Tommy Smith <tls.tommy11@gmail.com>
2018-05-03 08:19:18 -0500

    made calc

---
+++ Calculator/ViewController.designer.cs
@@ -0,0 +1,163 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace Calculator
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton button0 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton button1 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton button2 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton button3 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton button4 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton button5 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton button6 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton button7 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton button8 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton button9 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton buttonAdd { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton buttonChangeSign { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton buttonDecimal { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton buttonEqual { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton buttonMultiply { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton buttonSubtract { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (button0 != null) {
+                button0.Dispose ();
+                button0 = null;
+            }
+
+            if (button1 != null) {
+                button1.Dispose ();
+                button1 = null;
+            }
+
+            if (button2 != null) {
+                button2.Dispose ();
+                button2 = null;
+            }
+
+            if (button3 != null) {
+                button3.Dispose ();
+                button3 = null;
+            }
+
+            if (button4 != null) {
+                button4.Dispose ();
+                button4 = null;
+            }
+
+            if (button5 != null) {
+                button5.Dispose ();
+                button5 = null;
+            }
+
+            if (button6 != null) {
+                button6.Dispose ();
+                button6 = null;
+            }
+
+            if (button7 != null) {
+                button7.Dispose ();
+                button7 = null;
+            }
+
+            if (button8 != null) {
+                button8.Dispose ();
+                button8 = null;
+            }
+
+            if (button9 != null) {
+                button9.Dispose ();
+                button9 = null;
+            }
+
+            if (buttonAdd != null) {
+                buttonAdd.Dispose ();
+                buttonAdd = null;
+            }
+
+            if (buttonChangeSign != null) {
+                buttonChangeSign.Dispose ();
+                buttonChangeSign = null;
+            }
+
+            if (buttonDecimal != null) {
+                buttonDecimal.Dispose ();
+                buttonDecimal = null;
+            }
+
+            if (buttonEqual != null) {
+                buttonEqual.Dispose ();
+                buttonEqual = null;
+            }
+
+            if (buttonMultiply != null) {
+                buttonMultiply.Dispose ();
+                buttonMultiply = null;
+            }
+
+            if (buttonSubtract != null) {
+                buttonSubtract.Dispose ();
+                buttonSubtract = null;
+            }
+        }
+    }
+}
